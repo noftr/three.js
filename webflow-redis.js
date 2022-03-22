1e12584d64d7d28e5acdb9141fd2042b3c0083af
vyacheslav <vyacheslav.shejkin@gmail.com>
2022-03-22 23:07:00 +0300

    Update webflow-redis.js

---
+++ webflow-redis.js
@@ -0,0 +1,14 @@
+
+
+
+
+const e=document.createElement("canvas")
+
+  a.WebGLRenderer({canvas:e,context:t,alpha:!1,antialias:!1,stencil:!1,depth:!1,powerPreference:"high-performance"});
+
+  i.setPixelRatio(.01),
+  i.setSize(this.winSize.width,this.winSize.height),
+  i.outputEncoding=a.GammaEncoding,
+  i.autoClear=!1,
+  i.toneMapping=y.Linear,
+  i.shadowMap.type=a.PCFSoftShadowMap;
